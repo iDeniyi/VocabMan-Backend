4b052b9e63de0ba24a3b1b7ab84be5ed109a29f3
iDeniyi <inioluwa@ualberta.cai>
2024-02-13 16:50:17 -0700

    made a change to the backend to return information on starting the server

--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
+app.get("/", (req, res) => {
+    res.status(200).send("Testing response from the server!");
+});
