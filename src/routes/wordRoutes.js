010f990fa304d1d7fc28761ca7f460cb2951ea74
iDeniyi <inioluwa@ualberta.cai>
2024-02-18 21:15:56 -0700

    added a script for the cron job

--- src/routes/wordRoutes.js
+++ src/routes/wordRoutes.js
@@ -4,6 +4,5 @@ const router = express.Router();
 const wordController = require("../controllers/wordController");
 
 router.get("/word-of-the-day", wordController.getWordOfTheDay);
-router.put("/word-of-the-day", wordController.updateWordOfTheDay);
 
 module.exports = router;
